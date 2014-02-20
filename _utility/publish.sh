5027a8d805fb4fe006e1f49e3e6a13beb3575e86
Samir Talwar <samir@noodlesandwich.com>
2014-02-20 16:17:49 +0000

    GitHub demands a user name and email address for Travis.

--- _utility/publish.sh
+++ _utility/publish.sh
@@ -9,6 +9,9 @@ if ! [[ "$TRAVIS_REPO_SLUG" == 'codurance/site'
     return
 fi
 
+git config --global user.name 'Travis CI'
+git config --global user.email 'travis@travis-ci.org'
+
 git clone --quiet --branch=gh-pages "https://${GITHUB_TOKEN}@github.com/codurance/site.git" /tmp/_site > /dev/null
 bundle exec jekyll build --destination /tmp/_site
 
