69b8f5b2d272cf7637478a1546ed4081b55ccc16
Samir Talwar <samir@noodlesandwich.com>
2014-02-20 16:34:25 +0000

    Show me the error.

--- _utility/publish.sh
+++ _utility/publish.sh
@@ -13,7 +13,7 @@ git config --global user.name 'Travis CI'
 git config --global user.email 'travis@travis-ci.org'
 
 git clone --quiet --branch=gh-pages "https://${GITHUB_TOKEN}@github.com/codurance/site.git" /tmp/_site > /dev/null
-bundle exec jekyll build --destination /tmp/_site
+bundle exec jekyll build --destination /tmp/_site --trace
 
 cd /tmp/_site
 git add -A
