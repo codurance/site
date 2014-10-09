17b321f6208e19ce9c68e10571ba20ddccd6cfaa
Toby Retallick <toby.retallick@gmail.com>
2014-10-09 17:30:11 +0100

    Created client panel

--- assets/js/pages/index.js
+++ assets/js/pages/index.js
@@ -9,6 +9,37 @@ var Index = function () {
 			});
         },
 
+        // Owl carousel
+        initOwlCarousel: function () {
+		    $(document).ready(function() {
+ 
+				  var owl = $("#owl-demo");
+				 
+				  owl.owlCarousel({
+				      items : 5, //10 items above 1000px browser width
+				      itemsDesktop : [1000,5], //5 items between 1000px and 901px
+				      itemsDesktopSmall : [900,3], // betweem 900px and 601px
+				      itemsTablet: [600,2], //2 items between 600 and 0
+				      itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+				  });
+				 
+				  // Custom Navigation Events
+				  $(".next").click(function(){
+				    owl.trigger('owl.next');
+				  })
+				  $(".prev").click(function(){
+				    owl.trigger('owl.prev');
+				  })
+				  $(".play").click(function(){
+				    owl.trigger('owl.play',1000); //owl.play event accept autoPlay speed as second parameter
+				  })
+				  $(".stop").click(function(){
+				    owl.trigger('owl.stop');
+				  })
+				 
+				});
+				},
+
         //Layer Slider
         initLayerSlider: function () {
 		    $(document).ready(function(){
