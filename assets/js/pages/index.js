135d4fb0f127c9e61d993e3e3cb0593c1d4c9c6b
Toby Retallick <toby.retallick@gmail.com>
2014-10-10 12:07:02 +0100

    Added logos to client carousel and reformatted

--- assets/js/pages/index.js
+++ assets/js/pages/index.js
@@ -1,150 +1,135 @@
-var Index = function () {
+var Index = function() {
 
     return {
-        
+
         //Parallax Slider
-        initParallaxSlider: function () {
-			$(function() {
-				$('#da-slider').cslider();
-			});
+        initParallaxSlider: function() {
+            $(function() {
+                $('#da-slider').cslider();
+            });
         },
 
         // Owl carousel
-        initOwlCarousel: function () {
-		    $(document).ready(function() {
- 
-				  var owl = $("#owl-demo");
-				 
-				  owl.owlCarousel({
-				      items : 5, //10 items above 1000px browser width
-				      itemsDesktop : [1000,5], //5 items between 1000px and 901px
-				      itemsDesktopSmall : [900,3], // betweem 900px and 601px
-				      itemsTablet: [600,2], //2 items between 600 and 0
-				      itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
-				  });
-				 
-				  // Custom Navigation Events
-				  $(".next").click(function(){
-				    owl.trigger('owl.next');
-				  })
-				  $(".prev").click(function(){
-				    owl.trigger('owl.prev');
-				  })
-				  $(".play").click(function(){
-				    owl.trigger('owl.play',1000); //owl.play event accept autoPlay speed as second parameter
-				  })
-				  $(".stop").click(function(){
-				    owl.trigger('owl.stop');
-				  })
-				 
-				});
-				},
+        initOwlCarousel: function() {
+            $(document).ready(function() {
+
+                var owl = $("#owl-demo");
+
+                owl.owlCarousel({
+                    autoPlay: 3000, //Set AutoPlay to 3 seconds
+                    items: 3, //10 items above 1000px browser width
+                    itemsDesktop: [1000, 5], //5 items between 1000px and 901px
+                    itemsDesktopSmall: [900, 3], // betweem 900px and 601px
+                    itemsTablet: [600, 2], //2 items between 600 and 0
+                    itemsMobile: false // itemsMobile disabled - inherit from itemsTablet option
+                });
+            });
+        },
 
         //Layer Slider
-        initLayerSlider: function () {
-		    $(document).ready(function(){
-		        $('#layerslider').layerSlider({
-		            skinsPath : 'assets/plugins/layer_slider/skins/',
-		            skin : 'fullwidth',
-		            thumbnailNavigation : 'hover',
-		            hoverPrevNext : true,
-		            responsive : true,
-		            responsiveUnder : 960,
-		            sublayerContainer : 960
-		        });
-		    });     
+        initLayerSlider: function() {
+            $(document).ready(function() {
+                $('#layerslider').layerSlider({
+                    skinsPath: 'assets/plugins/layer_slider/skins/',
+                    skin: 'fullwidth',
+                    thumbnailNavigation: 'hover',
+                    hoverPrevNext: true,
+                    responsive: true,
+                    responsiveUnder: 960,
+                    sublayerContainer: 960
+                });
+            });
         },
 
         //Revolution Slider
-        initRevolutionSlider: function () {
-			var tpj=jQuery;
-			tpj.noConflict();
+        initRevolutionSlider: function() {
+            var tpj = jQuery;
+            tpj.noConflict();
 
-			tpj(document).ready(function() {
+            tpj(document).ready(function() {
 
-				if (tpj.fn.cssOriginal!=undefined)
-					tpj.fn.css = tpj.fn.cssOriginal;
+                if (tpj.fn.cssOriginal != undefined)
+                    tpj.fn.css = tpj.fn.cssOriginal;
 
-					var api = tpj('.fullwidthbanner').revolution(
-						{
-							delay:9000,
-							startwidth:960,
-							startheight:500,
+                var api = tpj('.fullwidthbanner').revolution({
+                    delay: 9000,
+                    startwidth: 960,
+                    startheight: 500,
 
-							onHoverStop:"off",						// Stop Banner Timet at Hover on Slide on/off
+                    onHoverStop: "off", // Stop Banner Timet at Hover on Slide on/off
 
-							thumbWidth:100,							// Thumb With and Height and Amount (only if navigation Tyope set to thumb !)
-							thumbHeight:50,
-							thumbAmount:3,
+                    thumbWidth: 100, // Thumb With and Height and Amount (only if navigation Tyope set to thumb !)
+                    thumbHeight: 50,
+                    thumbAmount: 3,
 
-							hideThumbs:1,
-							navigationType:"bullet",				// bullet, thumb, none
-							navigationArrows:"solo",				// nexttobullets, solo (old name verticalcentered), none
+                    hideThumbs: 1,
+                    navigationType: "bullet", // bullet, thumb, none
+                    navigationArrows: "solo", // nexttobullets, solo (old name verticalcentered), none
 
-							navigationStyle:"round-old",			// round,square,navbar,round-old,square-old,navbar-old, or any from the list in the docu (choose between 50+ different item), custom
-
-
-							navigationHAlign:"center",				// Vertical Align top,center,bottom
-							navigationVAlign:"bottom",				// Horizontal Align left,center,right
-							navigationHOffset:30,
-							navigationVOffset:20,
-
-							soloArrowLeftHalign:"left",
-							soloArrowLeftValign:"center",
-							soloArrowLeftHOffset:20,
-							soloArrowLeftVOffset:0,
-
-							soloArrowRightHalign:"right",
-							soloArrowRightValign:"center",
-							soloArrowRightHOffset:20,
-							soloArrowRightVOffset:0,
-
-							touchenabled:"on",						// Enable Swipe Function : on/off
+                    navigationStyle: "round-old", // round,square,navbar,round-old,square-old,navbar-old, or any from the list in the docu (choose between 50+ different item), custom
 
 
-							stopAtSlide:-1,							// Stop Timer if Slide "x" has been Reached. If stopAfterLoops set to 0, then it stops already in the first Loop at slide X which defined. -1 means do not stop at any slide. stopAfterLoops has no sinn in this case.
-							stopAfterLoops:-1,						// Stop Timer if All slides has been played "x" times. IT will stop at THe slide which is defined via stopAtSlide:x, if set to -1 slide never stop automatic
+                    navigationHAlign: "center", // Vertical Align top,center,bottom
+                    navigationVAlign: "bottom", // Horizontal Align left,center,right
+                    navigationHOffset: 30,
+                    navigationVOffset: 20,
 
-							hideCaptionAtLimit:0,					// It Defines if a caption should be shown under a Screen Resolution ( Basod on The Width of Browser)
-							hideAllCaptionAtLilmit:0,				// Hide all The Captions if Width of Browser is less then this value
-							hideSliderAtLimit:0,					// Hide the whole slider, and stop also functions if Width of Browser is less than this value
+                    soloArrowLeftHalign: "left",
+                    soloArrowLeftValign: "center",
+                    soloArrowLeftHOffset: 20,
+                    soloArrowLeftVOffset: 0,
 
+                    soloArrowRightHalign: "right",
+                    soloArrowRightValign: "center",
+                    soloArrowRightHOffset: 20,
+                    soloArrowRightVOffset: 0,
 
-							fullWidth:"on",
-
-							shadow:0								//0 = no Shadow, 1,2,3 = 3 Different Art of Shadows -  (No Shadow in Fullwidth Version !)
-
-						});
-
-
-						// TO HIDE THE ARROWS SEPERATLY FROM THE BULLETS, SOME TRICK HERE:
-						// YOU CAN REMOVE IT FROM HERE TILL THE END OF THIS SECTION IF YOU DONT NEED THIS !
-							api.bind("revolution.slide.onloaded",function (e) {
+                    touchenabled: "on", // Enable Swipe Function : on/off
 
 
-								jQuery('.tparrows').each(function() {
-									var arrows=jQuery(this);
+                    stopAtSlide: -1, // Stop Timer if Slide "x" has been Reached. If stopAfterLoops set to 0, then it stops already in the first Loop at slide X which defined. -1 means do not stop at any slide. stopAfterLoops has no sinn in this case.
+                    stopAfterLoops: -1, // Stop Timer if All slides has been played "x" times. IT will stop at THe slide which is defined via stopAtSlide:x, if set to -1 slide never stop automatic
 
-									var timer = setInterval(function() {
+                    hideCaptionAtLimit: 0, // It Defines if a caption should be shown under a Screen Resolution ( Basod on The Width of Browser)
+                    hideAllCaptionAtLilmit: 0, // Hide all The Captions if Width of Browser is less then this value
+                    hideSliderAtLimit: 0, // Hide the whole slider, and stop also functions if Width of Browser is less than this value
 
-										if (arrows.css('opacity') == 1 && !jQuery('.tp-simpleresponsive').hasClass("mouseisover"))
-										  arrows.fadeOut(300);
-									},3000);
-								})
 
-								jQuery('.tp-simpleresponsive, .tparrows').hover(function() {
-									jQuery('.tp-simpleresponsive').addClass("mouseisover");
-									jQuery('body').find('.tparrows').each(function() {
-										jQuery(this).fadeIn(300);
-									});
-								}, function() {
-									if (!jQuery(this).hasClass("tparrows"))
-										jQuery('.tp-simpleresponsive').removeClass("mouseisover");
-								})
-							});
-						// END OF THE SECTION, HIDE MY ARROWS SEPERATLY FROM THE BULLETS
+                    fullWidth: "on",
 
-			});
+                    shadow: 0 //0 = no Shadow, 1,2,3 = 3 Different Art of Shadows -  (No Shadow in Fullwidth Version !)
+
+                });
+
+
+                // TO HIDE THE ARROWS SEPERATLY FROM THE BULLETS, SOME TRICK HERE:
+                // YOU CAN REMOVE IT FROM HERE TILL THE END OF THIS SECTION IF YOU DONT NEED THIS !
+                api.bind("revolution.slide.onloaded", function(e) {
+
+
+                    jQuery('.tparrows').each(function() {
+                        var arrows = jQuery(this);
+
+                        var timer = setInterval(function() {
+
+                            if (arrows.css('opacity') == 1 && !jQuery('.tp-simpleresponsive').hasClass("mouseisover"))
+                                arrows.fadeOut(300);
+                        }, 3000);
+                    })
+
+                    jQuery('.tp-simpleresponsive, .tparrows').hover(function() {
+                        jQuery('.tp-simpleresponsive').addClass("mouseisover");
+                        jQuery('body').find('.tparrows').each(function() {
+                            jQuery(this).fadeIn(300);
+                        });
+                    }, function() {
+                        if (!jQuery(this).hasClass("tparrows"))
+                            jQuery('.tp-simpleresponsive').removeClass("mouseisover");
+                    })
+                });
+                // END OF THE SECTION, HIDE MY ARROWS SEPERATLY FROM THE BULLETS
+
+            });
         }
 
     };
