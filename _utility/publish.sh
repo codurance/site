97f72882a97e77b5f62cbea828a316b72fbe311e
Samir Talwar <samir@noodlesandwich.com>
2015-06-04 09:11:38 +0100

    Only run the build on *master*.

--- _utility/publish.sh
+++ _utility/publish.sh
@@ -2,13 +2,6 @@
 
 set -e
 
-if ! [[ "$TRAVIS_REPO_SLUG" == 'codurance/site'
-     && "$TRAVIS_PULL_REQUEST" == 'false'
-     && "$TRAVIS_BRANCH" == 'master'
-]]; then
-    exit 0
-fi
-
 git config --global user.name 'Travis CI'
 git config --global user.email 'travis@travis-ci.org'
 
