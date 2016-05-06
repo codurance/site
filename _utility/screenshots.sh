657434c05cc61f9b97706af8ea180de2787a6943
David Hatanian <david@hatanian.com>
2016-05-07 00:05:06 +0100

    Add imgur link to commit status

--- _utility/screenshots.sh
+++ _utility/screenshots.sh
@@ -7,4 +7,8 @@ echo "Taking screenshots with PhantomJS"
 phantomjs _utility/take-screenshot.js http://localhost:4000/blog/ blog.png 1200px*800px
 
 echo "Sending to imgur"
-_utility/imgurbash.sh blog.png
+SCREENSHOT_URL=`_utility/imgurbash.sh blog.png`
+
+echo "Updating commit status with screenshot at $SCREENSHOT_URL"
+curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://api.github.com/repos/codurance/site/statuses/$TRAVIS_COMMIT -d '{"state":"success","target_url":"$SCREENSHOT_URL","description":"Screenshot of the blog page","context":"continuous-integration/screenshot"}'
+
