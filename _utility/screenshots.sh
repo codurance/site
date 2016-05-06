17f17ab3f30dde28ba6c15de62727fb9d01b8acb
David Hatanian <david@hatanian.com>
2016-05-06 23:32:14 +0100

    Increase screen size as this looks awful otherwise

--- _utility/screenshots.sh
+++ _utility/screenshots.sh
@@ -4,7 +4,7 @@ echo "Starting Jekyll to take screenshots"
 bundle exec jekyll serve --no-watch --detach
 
 echo "Taking screenshots with PhantomJS"
-phantomjs _utility/take-screenshot.js http://localhost:4000/blog/ blog.png 800px*600px
+phantomjs _utility/take-screenshot.js http://localhost:4000/blog/ blog.png 1200px*800px
 
 echo "Sending to imgur"
 _utility/imgurbash.sh blog.png
