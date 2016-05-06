7cee55fb23cbdab196f7254bd6bee542d3b6829f
David Hatanian <david@hatanian.com>
2016-05-07 00:12:35 +0100

    Send imgur link in HTTPS

--- _utility/screenshots.sh
+++ _utility/screenshots.sh
@@ -8,7 +8,7 @@ phantomjs _utility/take-screenshot.js http://localhost:4000/blog/ blog.png 1200p
 
 echo "Sending to imgur"
 SCREENSHOT_URL=`_utility/imgurbash.sh blog.png`
-
+SCREENSHOT_URL_HTTPS=${SCREENSHOT_URL//http:\/\//https:\/\/}
 echo "Updating commit status with screenshot at $SCREENSHOT_URL"
-curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://api.github.com/repos/codurance/site/statuses/$TRAVIS_COMMIT -d '{"state":"success","target_url":"$SCREENSHOT_URL","description":"Screenshot of the blog page","context":"continuous-integration/screenshot"}'
+curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://api.github.com/repos/codurance/site/statuses/$TRAVIS_COMMIT -d '{"state":"success","target_url":"$SCREENSHOT_URL_HTTPS","description":"Screenshot of the blog page","context":"continuous-integration/screenshot"}'
 
