28bf9eb1c80e2742aed796d0926e32ceb66272a4
David Hatanian <david@hatanian.com>
2016-05-06 23:27:48 +0100

    send screenshot to imgur

--- _utility/screenshots.sh
+++ _utility/screenshots.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
+echo "Starting Jekyll to take screenshots"
 bundle exec jekyll serve --no-watch --detach
-curl http://localhost:4000/blog/
+
+echo "Taking screenshots with PhantomJS"
+phantomjs _utility/take-screenshot.js http://localhost:4000/blog/ blog.png 800px*600px
+
+echo "Sending to imgur"
+_utility/imgurbash.sh blog.png
