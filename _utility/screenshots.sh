2911ad8017d7c125ca0eedfd056b2d1710b20cec
David Hatanian <david@hatanian.com>
2016-05-07 00:19:42 +0100

    Check if this is a PR before taking screenshot

--- _utility/screenshots.sh
+++ _utility/screenshots.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-echo "Starting Jekyll to take screenshots"
-bundle exec jekyll serve --no-watch --detach
+if [ $TRAVIS_PULL_REQUEST = "true" ]
+then
+	echo "Starting Jekyll to take screenshots"
+	bundle exec jekyll serve --no-watch --detach
 
-echo "Taking screenshots with PhantomJS"
-phantomjs _utility/take-screenshot.js http://localhost:4000/blog/ blog.png 1200px*800px
+	echo "Taking screenshots with PhantomJS"
+	phantomjs _utility/take-screenshot.js http://localhost:4000/blog/ blog.png 1200px*800px
 
-echo "Sending to imgur"
-SCREENSHOT_URL=`_utility/imgurbash.sh blog.png`
-SCREENSHOT_URL_HTTPS=${SCREENSHOT_URL//http:\/\//https:\/\/}
-echo "Updating commit status with screenshot at $SCREENSHOT_URL"
-curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://api.github.com/repos/codurance/site/statuses/$TRAVIS_COMMIT -d '{"state":"success","target_url":"$SCREENSHOT_URL_HTTPS","description":"Screenshot of the blog page","context":"continuous-integration/screenshot"}'
-
+	echo "Sending to imgur"
+	SCREENSHOT_URL=`_utility/imgurbash.sh blog.png`
+	SCREENSHOT_URL_HTTPS=${SCREENSHOT_URL//http:\/\//https:\/\/}
+	
+	echo "Updating commit status with screenshot at $SCREENSHOT_URL_HTTPS"
+	curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://api.github.com/repos/codurance/site/statuses/$TRAVIS_COMMIT -d '{"state":"success","target_url":"$SCREENSHOT_URL_HTTPS","description":"Screenshot of the blog page","context":"continuous-integration/screenshot"}'
+fi
