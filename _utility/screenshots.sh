1ab42a70697d3fe90b94a687f3cd68945b1ef039
David Hatanian <david@hatanian.com>
2016-05-07 01:00:44 +0100

    Remove PR check on Travis for screenshots as it does not seem reliable

--- _utility/screenshots.sh
+++ _utility/screenshots.sh
@@ -3,17 +3,15 @@
 echo $TRAVIS_COMMIT_RANGE
 echo $TRAVIS_COMMIT
 
-if ! [[ "$TRAVIS_PULL_REQUEST" == 'false' ]]; then
-	echo "Starting Jekyll to take screenshots"
-	bundle exec jekyll serve --no-watch --detach
+echo "Starting Jekyll to take screenshots"
+bundle exec jekyll serve --no-watch --detach
 
-	echo "Taking screenshots with PhantomJS"
-	phantomjs _utility/take-screenshot.js http://localhost:4000/blog/ blog.png 1200px*800px
+echo "Taking screenshots with PhantomJS"
+phantomjs _utility/take-screenshot.js http://localhost:4000/blog/ blog.png 1200px*800px
 
-	echo "Sending to imgur"
-	SCREENSHOT_URL=`_utility/imgurbash.sh blog.png`
-	SCREENSHOT_URL_HTTPS=${SCREENSHOT_URL//http:\/\//https:\/\/}
+echo "Sending to imgur"
+SCREENSHOT_URL=`_utility/imgurbash.sh blog.png`
+SCREENSHOT_URL_HTTPS=${SCREENSHOT_URL//http:\/\//https:\/\/}
 
-	echo "Updating commit status with screenshot at $SCREENSHOT_URL_HTTPS"
-	curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://api.github.com/repos/codurance/site/statuses/$TRAVIS_COMMIT -d "{\"state\":\"success\",\"target_url\":\"$SCREENSHOT_URL_HTTPS\",\"description\":\"Screenshot of the blog page\",\"context\":\"continuous-integration/screenshot\"}"
-fi
+echo "Updating commit status with screenshot at $SCREENSHOT_URL_HTTPS"
+curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://api.github.com/repos/codurance/site/statuses/$TRAVIS_COMMIT -d "{\"state\":\"success\",\"target_url\":\"$SCREENSHOT_URL_HTTPS\",\"description\":\"Screenshot of the blog page\",\"context\":\"continuous-integration/screenshot\"}"
