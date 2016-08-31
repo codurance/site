08c00665612f01adbe3c7ba97a192e8827870c86
Mashooq Badar <mashooq.badar@gmail.com>
2016-08-31 19:06:36 +0100

    added index document setting

--- _utility/init_es_site.sh
+++ _utility/init_es_site.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 aws s3 mb s3://codurance-website-es --region $AWS_REGION
-#aws cloudfront create-distribution --distribution-config file://scripts/cloudfront-conf.json
+aws s3 website s3://codurance-website-es/ --index-document index.html
