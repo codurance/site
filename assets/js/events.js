6792a3b8c179f3ba5d4235791356278aa32ec5de
Halima <hkoundi@gmail.com>
2016-09-09 18:07:42 +0100

    retrieving lscc group events

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,21 +1,23 @@
 function getLsccMeetups(){
 	var $textElement = $('<textarea/>');
-	var apiEndpoint = 'https://api.meetup.com/Restarters-London/events?photo-host=public&page=3&sig_id=70197872&fields=plain_text_description&sig=a22300fd35fe388b3d8396babea82e4a5cb7e43e';
+	var apiEndpoint = 'https://api.meetup.com/london-software-craftsmanship/events?photo-host=public&page=3&sig_id=70197872&fields=plain_description&sig=c6bec32c7f1d44610ea27ef6576ea66c81ec314c';
 	var source   = (($textElement.html($("#entry-template").html())).text());
 	var template = Handlebars.compile(source);
-	console.log("getting lscc meetups");
+	
 	$.ajax({
 	     type : "GET",
 	     dataType : "jsonp",
 	     url : apiEndpoint,
 	     success: function(data){
-			$(data.data).each(function(i, lscc_event){
-				lscc_event.time = new Date(lscc_event.time).toDateString();
-				lscc_event.description = lscc_event.plain_text_description.substring(0,300) + "...";
-				var eventHtml = template(lscc_event);
-				$("#lssc_events").append($textElement.html(eventHtml).text());
-			});
-			
+				$(data.data).each(function(i, lscc_event){
+					lscc_event.time = new Date(lscc_event.time).toDateString();
+					lscc_event.description = lscc_event.plain_text_description.substring(0,300) + "...";
+					var eventHtml = template(lscc_event);
+					$("#lssc_events").append($textElement.html(eventHtml).text());
+				});
+				if($(data.data)){
+					$("#lssc_events").html("<li><h2>We're still planning a Meetup</h2></li>");
+				}
 	     		}
 	});
 }
