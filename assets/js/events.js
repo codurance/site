a0f672e3dca8462a8ca8caceb9d4bfec4a47a75c
Halima <hkoundi@gmail.com>
2016-09-14 10:38:15 +0100

    refactoring the javascript that displays lscc meetup

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,23 +1,37 @@
-function getLsccMeetups(){
-	var $textElement = $('<textarea/>');
-	var apiEndpoint = 'https://api.meetup.com/london-software-craftsmanship/events?photo-host=public&page=3&sig_id=70197872&fields=plain_description&sig=c6bec32c7f1d44610ea27ef6576ea66c81ec314c';
-	var source   = (($textElement.html($("#entry-template").html())).text());
-	var template = Handlebars.compile(source);
-	
+function listLsccMeetups(){	
+	executeApiRequest(displayEvents);
+}
+
+function executeApiRequest(callback){
+	var apiEndpoint = 'https://api.meetup.com/london-software-craftsmanship/events?photo-host=public&page=3&sig_id=70197872&fields=plain_text_description&sig=23964a489b632a356d3a23aaafa95dd93fb0d1b3';
 	$.ajax({
 	     type : "GET",
 	     dataType : "jsonp",
 	     url : apiEndpoint,
 	     success: function(data){
-				$(data.data).each(function(i, lscc_event){
-					lscc_event.time = new Date(lscc_event.time).toDateString();
-					lscc_event.description = lscc_event.plain_text_description.substring(0,300) + "...";
-					var eventHtml = template(lscc_event);
-					$("#lssc_events").append($textElement.html(eventHtml).text());
-				});
-				if($(data.data)){
-					$("#lssc_events").html("<li><h2>We're still planning a Meetup</h2></li>");
-				}
-	     		}
+				callback(data.data);
+			}
 	});
+}
+
+function displayEvents(data){
+	var $eventsTimelineElement = $("#lssc_events");
+	var $textElement = $('<textarea/>');
+	var source   = (($textElement.html($("#entry-template").html())).text());
+	var template = Handlebars.compile(source);
+
+	$(data).each(function(i, lscc_event){
+		lscc_event.time = new Date(lscc_event.time).toDateString();
+		lscc_event.description = lscc_event.plain_text_description.substring(0,300) + "...";
+		var eventHtml = template(lscc_event);
+		$eventsTimelineElement.append($textElement.html(eventHtml).text());
+	});
+
+	if(!$(data) || $(data).length == 0){
+		displayDefaultMessage($eventsTimelineElement);
+	}
+}
+
+function displayDefaultMessage(eventsTimelineElement){
+	eventsTimelineElement.html("<li><h2>We are still planning the next meetup, come back soon for more!</h2></li>");
 }
