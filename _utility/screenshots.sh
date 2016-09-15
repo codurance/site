0dd2ac8bdc16be67f546943065537f46031c53f6
Carlos Morera de la Chica <carlosdelachica@gmail.com>
2016-09-15 14:31:31 +0100

    Update screenshot script

--- _utility/screenshots.sh
+++ _utility/screenshots.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
-echo $TRAVIS_COMMIT_RANGE
-echo $TRAVIS_COMMIT
-
 echo "Starting Jekyll to take screenshots"
 bundle exec jekyll serve --no-watch --detach
 
 echo "Taking screenshots with PhantomJS"
-phantomjs _utility/take-screenshot.js http://localhost:4000/blog/ blog.png 1200px*800px
+phantomjs _utility/take-screenshot.js http://localhost:4000/publications publications.png 1200px*800px
 
 echo "Sending to imgur"
 SCREENSHOT_URL=`_utility/imgurbash.sh blog.png`
