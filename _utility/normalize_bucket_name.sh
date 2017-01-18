7ea6e470e1a8d2db7d8bbe35b71de6a19ccd928a
Christian Panadero <panavtec@gmail.com>
2017-01-18 11:08:11 +0000

    remove unnecesary function

--- _utility/normalize_bucket_name.sh
+++ _utility/normalize_bucket_name.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 set -e
 
-normalizedBucketName() {
-  LOWERCASE_BUCKET_NAME=${1,,}
-  echo ${LOWERCASE_BUCKET_NAME//_/-}
-}
-
-normalizedBucketName $1
+LOWERCASE_BUCKET_NAME=${1,,}
+echo ${LOWERCASE_BUCKET_NAME//_/-}
