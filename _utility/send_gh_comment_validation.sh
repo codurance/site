5c5c95cfde78180833de9655209479ec8b665225
Christian Panadero <panavtec@gmail.com>
2017-01-30 16:58:57 +0000

    split validation from actual script

--- _utility/send_gh_comment_validation.sh
+++ _utility/send_gh_comment_validation.sh
@@ -1,12 +1,3 @@
-#!/bin/bash
-set +e
-
-REPO_OWNER=''
-REPO_NAME=''
-PR_NUMBER=''
-GITHUB_TOKEN=''
-DEPLOYMENT_URL=''
- 
 # Define help function
 function help(){
     echo "deploy_prb";
@@ -111,36 +102,4 @@ if [ -z "$DEPLOYMENT_URL" ]
 then
     echo "DEPLOYMENT_URL is required";
     help;
-fi
-
-if [ -z "$PR_NUMBER" ]
-then
-	PR_NUMBER=$(curl -X GET -u ${GITHUB_TOKEN}:x-oauth-basic "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/pulls?head=${REPO_OWNER}:${CIRCLE_BRANCH}" | jq ".[0].number")
-	if [ -z "$PR_NUMBER" ]
-	then
-		echo "PR_NUMBER not specified, and can't be determined in the current context"
-		exit 1
-	fi
-fi
-
-LAST_COMMENT_ID=$(curl https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/issues/$PR_NUMBER/comments?access_token=$GITHUB_TOKEN \
-	-X GET | jq "[.[] | select(.user.login==\"$GITHUB_USERNAME\")][0].id")
-if [ $? -ne 0 ]; then
-  echo "Error while parsing last commend id"
-  exit 1
-fi
-
-COMMENT="Deployed: $DEPLOYMENT_URL"
-if [ "$LAST_COMMENT_ID" != "null" ]; then
-	echo "Found a previous comment with id:$LAST_COMMENT_ID . Updating comment"
-	curl https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/issues/comments/$LAST_COMMENT_ID?access_token=$GITHUB_TOKEN \
-		-H "Content-Type: application/json" \
-		-X POST \
-		-d "{ \"body\":\"$COMMENT\" }"
-else
-	echo "Creating a comment with deployment url"
-	curl https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/issues/$PR_NUMBER/comments?access_token=$GITHUB_TOKEN \
-		-H "Content-Type: application/json" \
-		-X POST \
-		-d "{ \"body\":\"$COMMENT\" }"
 fi
