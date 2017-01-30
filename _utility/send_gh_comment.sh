4eb560b740a7ac49768c404093099a50f5e6c6d4
Christian Panadero <panavtec@gmail.com>
2017-01-30 16:39:30 +0000

    send gh comment updated script

--- _utility/send_gh_comment.sh
+++ _utility/send_gh_comment.sh
@@ -1,34 +1,146 @@
 #!/bin/bash
-set -e
+set +e
 
-if [ "$#" -ne 5 ]; then
-  echo "Insufficient arguments, see usage and example for further assistance."
-  echo -e "Usage: $0 REPO_OWNER REPO_NAME PR_NUMBER AUTH_TOKEN DEPLOYMENT_URL\n"
-  echo "Example: $0 codurance site 4 token https://bucket-name.amazon.com/index.html"
+REPO_OWNER=''
+REPO_NAME=''
+PR_NUMBER=''
+GITHUB_TOKEN=''
+DEPLOYMENT_URL=''
+ 
+# Define help function
+function help(){
+    echo "deploy_prb";
+    echo "Usage example:";
+    echo "deploy-prb (-o|--REPO_OWNER) string (-n|--REPO_NAME) string (-g|--GITHUB_TOKEN) string (-d|--DEPLOYMENT_URL) string [(-h|--HELP)] [(-p|--PR_NUMBER) string]";
+    echo "Options:";
+    echo "-h or --HELP: Displays this information.";
+    echo "-o or --REPO_OWNER string: Repository owner. Required.";
+    echo "-n or --REPO_NAME string: Repository name. Required.";
+    echo "-p or --PR_NUMBER string: Pull requeset number.";
+    echo "-g or --GITHUB_TOKEN string: Github token. Required.";
+    echo "-d or --DEPLOYMENT_URL string: Deployment url. Required.";
+    exit 1;
+}
+ 
+# Declare vars. Flags initalizing to 0.
+ 
+# Execute getopt
+ARGS=$(getopt -o "ho:n:p:g:d:" -l "HELP,REPO_OWNER:,REPO_NAME:,PR_NUMBER:,GITHUB_TOKEN:,DEPLOYMENT_URL:" -n "deploy_prb" -- "$@");
+ 
+#Bad arguments
+if [ $? -ne 0 ];
+then
+    help;
+fi
+ 
+eval set -- "$ARGS";
+ 
+while true; do
+    case "$1" in
+        -h|--HELP)
+            shift;
+            help;
+            ;;
+        -o|--REPO_OWNER)
+            shift;
+                    if [ -n "$1" ]; 
+                    then
+                        REPO_OWNER="$1";
+                        shift;
+                    fi
+            ;;
+        -n|--REPO_NAME)
+            shift;
+                    if [ -n "$1" ]; 
+                    then
+                        REPO_NAME="$1";
+                        shift;
+                    fi
+            ;;
+        -p|--PR_NUMBER)
+            shift;
+                    if [ -n "$1" ]; 
+                    then
+                        PR_NUMBER="$1";
+                        shift;
+                    fi
+            ;;
+        -g|--GITHUB_TOKEN)
+            shift;
+                    if [ -n "$1" ]; 
+                    then
+                        GITHUB_TOKEN="$1";
+                        shift;
+                    fi
+            ;;
+        -d|--DEPLOYMENT_URL)
+            shift;
+                    if [ -n "$1" ]; 
+                    then
+                        DEPLOYMENT_URL="$1";
+                        shift;
+                    fi
+            ;;
+ 
+        --)
+            shift;
+            break;
+            ;;
+    esac
+done
+ 
+if [ -z "$REPO_OWNER" ]
+then
+    echo "REPO_OWNER is required";
+    help;
+fi
+ 
+if [ -z "$REPO_NAME" ]
+then
+    echo "REPO_NAME is required";
+    help;
+fi
+ 
+if [ -z "$GITHUB_TOKEN" ]
+then
+    echo "GITHUB_TOKEN is required";
+    help;
+fi
+ 
+if [ -z "$DEPLOYMENT_URL" ]
+then
+    echo "DEPLOYMENT_URL is required";
+    help;
+fi
+
+if [ -z "$PR_NUMBER" ]
+then
+	PR_NUMBER=$(curl -X GET -u ${GITHUB_TOKEN}:x-oauth-basic "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/pulls?head=${REPO_OWNER}:${CIRCLE_BRANCH}" | jq ".[0].number")
+	if [ -z "$PR_NUMBER" ]
+	then
+		echo "PR_NUMBER not specified, and can't be determined in the current context"
+		exit 1
+	fi
+fi
+
+LAST_COMMENT_ID=$(curl https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/issues/$PR_NUMBER/comments?access_token=$GITHUB_TOKEN \
+	-X GET | jq "[.[] | select(.user.login==\"$GITHUB_USERNAME\")][0].id")
+if [ $? -ne 0 ]; then
+  echo "Error while parsing last commend id"
   exit 1
 fi
 
-GITHUB_USERNAME='CoduranceBot'
-REPO_OWNER=$1
-REPO_NAME=$2
-PR_NUMBER=$3
-AUTH_TOKEN=$4
-DEPLOYMENT_URL=$5
-
 COMMENT="Deployed: $DEPLOYMENT_URL"
-LAST_COMMENT_ID=$(curl https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/issues/$PR_NUMBER/comments?access_token=$AUTH_TOKEN \
-  -X GET | jq "[.[] | select(.user.login==\"$GITHUB_USERNAME\")][0].id")
-
 if [ "$LAST_COMMENT_ID" != "null" ]; then
-  echo "Found a previous comment with id:$LAST_COMMENT_ID . Updating comment"
-  curl https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/issues/comments/$LAST_COMMENT_ID?access_token=$AUTH_TOKEN \
-    -H "Content-Type: application/json" \
-    -X POST \
-    -d "{ \"body\":\"$COMMENT\" }"
+	echo "Found a previous comment with id:$LAST_COMMENT_ID . Updating comment"
+	curl https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/issues/comments/$LAST_COMMENT_ID?access_token=$GITHUB_TOKEN \
+		-H "Content-Type: application/json" \
+		-X POST \
+		-d "{ \"body\":\"$COMMENT\" }"
 else
-  echo "Creating a comment with deployment url"
-  curl https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/issues/$PR_NUMBER/comments?access_token=$AUTH_TOKEN \
-    -H "Content-Type: application/json" \
-    -X POST \
-    -d "{ \"body\":\"$COMMENT\" }"
+	echo "Creating a comment with deployment url"
+	curl https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/issues/$PR_NUMBER/comments?access_token=$GITHUB_TOKEN \
+		-H "Content-Type: application/json" \
+		-X POST \
+		-d "{ \"body\":\"$COMMENT\" }"
 fi
