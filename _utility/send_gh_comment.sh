060c68317e1ad7b9e36db09ee26da3d5b5e6f8d3
Christian Panadero <panavtec@gmail.com>
2017-02-06 10:52:34 +0000

    extracted send_gh_comment into functions

--- _utility/send_gh_comment.sh
+++ _utility/send_gh_comment.sh
@@ -7,35 +7,52 @@ PR_NUMBER=''
 GITHUB_TOKEN=''
 DEPLOYMENT_URL=''
 GITHUB_USERNAME='CoduranceBot'
+COMMENT="Deployed: $DEPLOYMENT_URL"
 
 DIR="$( cd $( dirname "${BASH_SOURCE[0]}" ) && pwd )"
 source "$DIR/send_gh_comment_validation.sh"
 
-if [ -z "$PR_NUMBER" ]; then
+function get_pr_number_by_branch_name() {
   PR_NUMBER=$(curl -X GET -u ${GITHUB_TOKEN}:x-oauth-basic "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/pulls?head=${REPO_OWNER}:${CIRCLE_BRANCH}" | jq ".[0].number")
 	if [ -z "$PR_NUMBER" ]; then
 		echo "PR_NUMBER not specified, and can't be determined in the current context"
 		exit 1
 	fi
-fi
+}
 
-LAST_COMMENT_ID=$(curl https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/issues/$PR_NUMBER/comments?access_token=$GITHUB_TOKEN -X GET | jq "[.[] | select(.user.login==\"$GITHUB_USERNAME\")][0].id")
-if [ $? -ne 0 ]; then
-  echo "Error while parsing last commend id"
-  exit 1
-fi
+function get_last_comment_id_generated_by_bot() {
+  LAST_COMMENT_ID=$(curl https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/issues/$PR_NUMBER/comments?access_token=$GITHUB_TOKEN -X GET | jq "[.[] | select(.user.login==\"$GITHUB_USERNAME\")][0].id")
+  if [ $? -ne 0 ]; then
+    echo "Error while parsing last commend id"
+    exit 1
+  fi
+  echo $LAST_COMMENT_ID
+}
 
-COMMENT="Deployed: $DEPLOYMENT_URL"
-if [ "$LAST_COMMENT_ID" != "null" ]; then
+function update_last_comment() {
 	echo "Found a previous comment with id:$LAST_COMMENT_ID . Updating comment"
 	curl https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/issues/comments/$LAST_COMMENT_ID?access_token=$GITHUB_TOKEN \
 		-H "Content-Type: application/json" \
 		-X POST \
 		-d "{ \"body\":\"$COMMENT\" }"
-else
+}
+
+function create_comment() {
 	echo "Creating a comment with deployment url"
 	curl https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/issues/$PR_NUMBER/comments?access_token=$GITHUB_TOKEN \
 		-H "Content-Type: application/json" \
 		-X POST \
 		-d "{ \"body\":\"$COMMENT\" }"
+}
+
+if [ -z "$PR_NUMBER" ]; then
+  get_pr_number_by_branch_name;
 fi
+
+LAST_COMMENT_ID=$(get_last_comment_id_generated_by_bot);
+if [ "$LAST_COMMENT_ID" == "null" ]; then
+  create_comment;
+else
+  update_last_comment;
+fi
+
