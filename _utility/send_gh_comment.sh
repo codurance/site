0e2536981876576ee31f2059a636e6e8d26b122a
Christian Panadero <panavtec@gmail.com>
2017-02-06 10:05:08 +0000

    remove unwanted quote

--- _utility/send_gh_comment.sh
+++ _utility/send_gh_comment.sh
@@ -8,7 +8,7 @@ GITHUB_TOKEN=''
 DEPLOYMENT_URL=''
 GITHUB_USERNAME='CoduranceBot'
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$( cd $( dirname "${BASH_SOURCE[0]}" ) && pwd )"
 source "$DIR/send_gh_comment_validation.sh"
 
 if [ -z "$PR_NUMBER" ]; then
