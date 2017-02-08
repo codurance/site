9fce8234c1c3b3bd3901359b1bf4d708ad0e4234
Christian Panadero <panavtec@gmail.com>
2017-02-08 17:21:46 +0000

    Set baseurl to empty in master

--- _utility/deploy_es_site.sh
+++ _utility/deploy_es_site.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-cd _site/es
+cd _site_es/
 aws s3 sync . s3://codurance-website-es --acl public-read
 aws cloudfront create-invalidation --distribution-id E12HY50RS84ICW --paths "/*"
 cd ../..
