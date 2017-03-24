7cc1ef8bbdcd9bf0df9d8d83d25a869f597a1b61
Sean Newham <sean@codurance.com>
2017-03-24 11:06:05 +0000

    Seeing if this works in theory the other way around, as the site doesn't appear to be built the same for production as test.

--- _utility/build_site_prb.sh
+++ _utility/build_site_prb.sh
@@ -8,6 +8,9 @@ if [[ "$FOLDER_NAME" != "site-master" ]]; then
   echo "baseurl: /$FOLDER_NAME" > _config_prb.yml
   cat _config_prb.yml
   rake buildenprb
+   export enUrl=$(cat _config.yml | shyaml domains.en)
+  export esUrl=$(cat _config.yml | shyaml domains.es)
+  sed -Ei '' 's/$snUrl/$enUrl/g' ./_site/sitemap.xml
 else
   echo "baseurl: ''" > _config_prb.yml
   rake buildesprb
