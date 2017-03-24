65ba69dc0713a36e6d3288c89f7c366d6fdcdcf9
Sean Newham <sean@codurance.com>
2017-03-24 13:31:49 +0000

    Playing with the command.

--- _utility/build_site_prb.sh
+++ _utility/build_site_prb.sh
@@ -11,7 +11,7 @@ if [[ "$FOLDER_NAME" != "site-master" ]]; then
   pip install shyaml
   export enUrl=$(cat _config.yml | shyaml get-value domains.en)
   export esUrl=$(cat _config.yml | shyaml get-value domains.es)
-  sed -i 's/$enUrl/$esUrl/g' ./_site/sitemap.xml
+  sed -i 's/${enUrl}/${esUrl}/g' ./_site/sitemap.xml
 else
   echo "baseurl: ''" > _config_prb.yml
   rake buildesprb
@@ -19,7 +19,7 @@ else
   pip install shyaml
   export enUrl=$(cat _config.yml | shyaml get-value domains.en)
   export esUrl=$(cat _config.yml | shyaml get-value domains.es)
-  sed -i 's/$esUrl/$enUrl/g' ./_site_es/sitemap.xml
+  sed -i 's/${esUrl}/${enUrl}/g' ./_site_es/sitemap.xml
 
 fi
 
