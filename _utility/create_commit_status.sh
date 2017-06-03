346c005087f2a6e17ded6f4b842be636b6a45b57
Christian Panadero <panavtec@gmail.com>
2017-06-03 15:16:58 +0100

    Try to update commit status instead sending comment

---
+++ _utility/create_commit_status.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set +e
+
+REPO_OWNER=$1
+REPO_NAME=$2
+GITHUB_TOKEN=$3
+DEPLOYMENT_URL=$4
+COMMIT_SHA=$5
+
+curl "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/statuses/$COMMIT_SHA?access_token=$GITHUB_TOKEN" \
+  -H "Content-Type: application/json" \
+  -X POST \
+  -d "{
+        \"state\":\"success\"
+        \"target_url\": \"$DEPLOYMENT_URL\"
+        \"description\": \"Deployed preview\"
+        \"context\": \"codurance/preview\"
+      }"
+
