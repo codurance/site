1fc1a51f9d57f15d45e2b68d419bfb554c5dab4c
Alfredo Fernandez <alfredodev@Alfredos-MacBook-Pro.local>
2018-09-17 15:04:14 +0100

    Fixed file settings by language

--- _utility/build_site_prb.sh
+++ _utility/build_site_prb.sh
@@ -6,14 +6,15 @@ FOLDER_NAME=$1
 echo "Building website for: $FOLDER_NAME"
 
 if [[ "$FOLDER_NAME" != "site-master" ]]; then
-  echo "baseurl: /$FOLDER_NAME/en" > _config_en.yml
-  echo "baseurl: /$FOLDER_NAME/es" > _config_es.yml
+  echo "baseurl: /$FOLDER_NAME/en" >> _config_en.yml
+  echo "baseurl: /$FOLDER_NAME/es" >> _config_es.yml
 else
   echo "baseurl: ''" > _config_prb.yml
 fi
 
 rake buildesprb
 rake buildenprb
+
 pip install shyaml
 export enUrl=$(cat _config.yml | shyaml get-value domains.en)
 export esUrl=$(cat _config.yml | shyaml get-value domains.es)
