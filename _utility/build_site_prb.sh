e8cf93a4234120513e5f285e673f580097b91b81
Alfredo Fernandez <alfredodev@Alfredos-MacBook-Pro.local>
2018-09-17 11:29:51 +0100

    Changed config scripts to deploy es version too

--- _utility/build_site_prb.sh
+++ _utility/build_site_prb.sh
@@ -4,18 +4,18 @@ set -eux
 FOLDER_NAME=$1
 
 echo "Building website for: $FOLDER_NAME"
+
 if [[ "$FOLDER_NAME" != "site-master" ]]; then
   echo "baseurl: /$FOLDER_NAME" > _config_prb.yml
-  cat _config_prb.yml
-  rake buildenprb
 else
   echo "baseurl: ''" > _config_prb.yml
-  rake buildesprb
-  rake buildenprb
-  pip install shyaml
-  export enUrl=$(cat _config.yml | shyaml get-value domains.en)
-  export esUrl=$(cat _config.yml | shyaml get-value domains.es)
-  sed -i "s#${enUrl}#${esUrl}#g" ./_site_es/sitemap.xml
+
+rake buildesprb
+rake buildenprb
+pip install shyaml
+export enUrl=$(cat _config.yml | shyaml get-value domains.en)
+export esUrl=$(cat _config.yml | shyaml get-value domains.es)
+sed -i "s#${enUrl}#${esUrl}#g" ./_site_es/sitemap.xml
 
 fi
 
--- _utility/deploy_prb.sh
+++ _utility/deploy_prb.sh
@@ -12,7 +12,15 @@ FOLDER_NAME=$1
 
 cd _site/
 
-echo "Synchronizing to folder: $FOLDER_NAME"
-aws s3 sync . s3://codurance-site-pr/$FOLDER_NAME --acl public-read
-aws cloudfront create-invalidation --distribution-id EML2BUMD54HSC --paths "/$FOLDER_NAME/*"
+echo "Synchronizing to folder: $FOLDER_NAME/en"
+aws s3 sync . s3://codurance-site-pr/$FOLDER_NAME/en --exclude "es/*" --acl public-read
+aws cloudfront create-invalidation --distribution-id EML2BUMD54HSC --paths "/$FOLDER_NAME/en"
+
+
+cd ../..
+cd _site_es/
+echo "Synchronizing to folder: $FOLDER_NAME/es"
+aws s3 sync . s3://codurance-site-pr/$FOLDER_NAME/es --acl public-read
+aws cloudfront create-invalidation --distribution-id EML2BUMD54HSC --paths "/$FOLDER_NAME/es"
+
 
