ea874db55a03ebf90e5179dd135ac9b167300e6d
Alfredo Fernandez <alfredodev@Alfredos-MacBook-Pro.local>
2018-09-17 14:13:34 +0100

    Fixed baseUrl config by language

--- _utility/build_site_prb.sh
+++ _utility/build_site_prb.sh
@@ -6,7 +6,8 @@ FOLDER_NAME=$1
 echo "Building website for: $FOLDER_NAME"
 
 if [[ "$FOLDER_NAME" != "site-master" ]]; then
-  echo "baseurl: /$FOLDER_NAME" > _config_prb.yml
+  echo "baseurl: /$FOLDER_NAME/en" > _config_en.yml
+  echo "baseurl: /$FOLDER_NAME/es" > _config_es.yml
 else
   echo "baseurl: ''" > _config_prb.yml
 fi
