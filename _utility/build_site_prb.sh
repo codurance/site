e8b0529c103a1473c6da259be475dd5f0f59c0a9
Alfredo Fernandez <alfredodev@Alfredos-MacBook-Pro.local>
2018-09-20 17:47:15 +0100

    tests

--- _utility/build_site_prb.sh
+++ _utility/build_site_prb.sh
@@ -8,8 +8,8 @@ if [[ "$FOLDER_NAME" != "site-master" ]]; then
   echo "baseurl: /$FOLDER_NAME" > _config_prb.yml
   cat _config_prb.yml
   rake buildenprb
-  $(ruby ./_utility/read_yaml_value.rb "_config.yml" "domains.en")	
-  $(ruby r./_utility/ead_yaml_value.rb "_config.yml" "domains.es")
+  ruby ./_utility/read_yaml_value.rb "_config.yml" "domains.en"
+  ruby ./_utility/ead_yaml_value.rb "_config.yml" "domains.es"
 else
   echo "baseurl: ''" > _config_prb.yml
   rake buildesprb
