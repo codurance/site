d69817ecb0004f1af9bce601094da9cdce3ebb6b
Alfredo Fernandez <alfredodev@Alfredos-MacBook-Pro.local>
2018-10-08 14:30:24 +0100

    Initial restructure > docker works localy

--- build/_utility/build_site_prb.sh
+++ build/_utility/build_site_prb.sh
@@ -8,14 +8,14 @@ if [[ "$FOLDER_NAME" != "site-master" ]]; then
   echo "baseurl: /$FOLDER_NAME" > _config_prb.yml
   cat _config_prb.yml
   rake buildenprb
-  ruby ./_utility/read_yaml_value.rb "_config.yml" "domains.en"
-  ruby ./_utility/read_yaml_value.rb "_config.yml" "domains.es"
+  ruby ./_utility/read_yaml_value.rb "config/_config.yml" "domains.en"
+  ruby ./_utility/read_yaml_value.rb "config/_config.yml" "domains.es"
 else
   echo "baseurl: ''" > _config_prb.yml
   rake buildesprb
   rake buildenprb
-  export enUrl=$(ruby ./_utility/read_yaml_value.rb "_config.yml" "domains.en")	
-  export esUrl=$(ruby ./_utility/read_yaml_value.rb "_config.yml" "domains.es")
+  export enUrl=$(ruby ./_utility/read_yaml_value.rb "config/_config.yml" "domains.en")	
+  export esUrl=$(ruby ./_utility/read_yaml_value.rb "config/_config.yml" "domains.es")
   sed -i "s#${enUrl}#${esUrl}#g" ./_site_es/sitemap.xml
 fi
 
