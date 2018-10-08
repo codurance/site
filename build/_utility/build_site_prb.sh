1b520227d67d95a75396bb1969c23ea520f40625
Alfredo Fernandez <alfredodev@Alfredos-MacBook-Pro.local>
2018-10-08 15:38:52 +0100

    Fixed build script paths

--- build/_utility/build_site_prb.sh
+++ build/_utility/build_site_prb.sh
@@ -8,14 +8,14 @@ if [[ "$FOLDER_NAME" != "site-master" ]]; then
   echo "baseurl: /$FOLDER_NAME" > build/config/_config_prb.yml
   cat build/config/_config_prb.yml
   rake buildenprb
-  ruby .build/_utility/read_yaml_value.rb "build/config/_config.yml" "domains.en"
-  ruby .build/_utility/read_yaml_value.rb "build/config/_config.yml" "domains.es"
+  ruby build/_utility/read_yaml_value.rb "build/config/_config.yml" "domains.en"
+  ruby build/_utility/read_yaml_value.rb "build/config/_config.yml" "domains.es"
 else
   echo "baseurl: ''" > build/config/_config_prb.yml
   rake buildesprb
   rake buildenprb
-  export enUrl=$(ruby .build/_utility/read_yaml_value.rb "build/config/_config.yml" "domains.en")	
-  export esUrl=$(ruby .build/_utility/read_yaml_value.rb "build/config/_config.yml" "domains.es")
+  export enUrl=$(ruby build/_utility/read_yaml_value.rb "build/config/_config.yml" "domains.en")	
+  export esUrl=$(ruby build/_utility/read_yaml_value.rb "build/config/_config.yml" "domains.es")
   sed -i "s#${enUrl}#${esUrl}#g" ./output/_site_es/sitemap.xml
 fi
 
