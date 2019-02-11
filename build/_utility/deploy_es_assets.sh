8e231b3b8eb16f105d2119efe5efff89c7643932
Andre Torres <andre.torres@codurance.com>
2019-02-11 11:44:07 +0000

    Remove unnecessary cd

--- build/_utility/deploy_es_assets.sh
+++ build/_utility/deploy_es_assets.sh
@@ -7,4 +7,3 @@ cd output/_site_es/
 aws s3 sync ./assets s3://codurance-website-es/assets --acl public-read --exclude "*" --include "*.js" --include "*.css"
 
 aws cloudfront create-invalidation --distribution-id E12HY50RS84ICW --paths "/assets/*"
-cd ../..
--- build/_utility/deploy_es_site.sh
+++ build/_utility/deploy_es_site.sh
@@ -5,4 +5,3 @@ set -e
 cd output/_site_es/
 aws s3 sync . s3://codurance-website-es --exclude "assets/*" --acl public-read
 aws cloudfront create-invalidation --distribution-id E12HY50RS84ICW --paths "/*"
-cd ../../..
