4d738137965b6e620d826ac20e8bed28ed6eccb6
Francesco Gigli <jaramir@gmail.com>
2019-02-13 11:59:46 +0100

    detect colliding slugs

--- src/_plugins/jekyll_enforce_tags_casing.rb
+++ src/_plugins/jekyll_enforce_tags_casing.rb
@@ -1,10 +1,11 @@
 Jekyll::Hooks.register :site, :post_read do |site|
   tags = site.tags.keys
-  uniqueTags = tags.uniq(&:downcase)
-  duplicates = tags - uniqueTags
-  
-  if duplicates.size > 0
-    raise "There are duplicated tags with different casing. Please check and fix the tags '#{duplicates}'"
-  end
+  unique_tags = tags.uniq {|tag| Jekyll::Utils.slugify(tag)}
+  duplicates = tags - unique_tags
 
+  if duplicates.size > 0
+    raise "There are tags with colliding slugs.
+        Please deduplicate these tags:
+          #{duplicates.join("\n          ")}"
+  end
 end
