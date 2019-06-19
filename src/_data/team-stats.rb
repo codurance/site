536b123b2c6f04aca4f4456ee43e3167294291c2
Francesco Gigli <jaramir@gmail.com>
2019-06-19 14:02:29 +0200

    extracting a count of roles for each office

---
+++ src/_data/team-stats.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+
+require 'yaml'
+
+Dir.chdir File.dirname __FILE__
+
+role_aliases = {
+  "Software Craftsman and Co-Founder" => "Software Craftspeople",
+  "UX Craftswoman" => "UX Craftspeople",
+  "Software Craftsman" => "Software Craftspeople",
+  "Software Craftswoman" => "Software Craftspeople",
+  "Software Craftsperson" => "Software Craftspeople",
+  "Head of Recruitment (UK)" => "Head of Recruitment",
+  "Head of Sales (Spain)" => "Head of Sales",
+  "Head of Professional Services (Spain)" => "Head of Professional Services"
+}
+
+team = YAML.load_file('team.yml').map { |person|
+  person['role'] = role_aliases[person['role']] || person['role']
+  person
+}
+
+roles = team.map { |person| person['role'] }.uniq
+offices = team.map { |person| person['office'] }.uniq
+
+# sites = team.map { |p| p[:] }.uniq
+offices.each { |office|
+  puts office
+  roles.each { |role|
+    count = team.count { |person| person['role'] == role and person['office'] == office }
+    if count > 0 then
+      puts "#{count} #{role}"
+    end
+  }
+}
