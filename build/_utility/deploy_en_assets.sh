82c20a1dce2ddea239a864181dd3b1f87b8bf3c8
Matt Gray <mattgray83@hotmail.co.uk>
2020-02-21 14:47:36 +0000

    Adds --delete option to all S3 sync commands

--- build/_utility/deploy_en_assets.sh
+++ build/_utility/deploy_en_assets.sh
@@ -4,6 +4,6 @@ set -e
 
 cd output/_site
 
-aws s3 sync ./assets s3://codurance-website-en/assets --acl public-read --exclude "*" --include "*.js" --include "*.css"
+aws s3 sync ./assets s3://codurance-website-en/assets --acl public-read --exclude "*" --include "*.js" --include "*.css" --delete
 
 aws cloudfront create-invalidation --distribution-id EML2BUMD54HSC --paths "/assets/*"
