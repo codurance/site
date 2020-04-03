3a26aa378d144c473826b023b3ca8e9567bf632e
Matt Gray <mattgray83@hotmail.co.uk>
2020-04-03 16:09:19 +0100

    Move mobileClickHandler to global

--- src/assets/custom/js/softmod-tab-control.js
+++ src/assets/custom/js/softmod-tab-control.js
@@ -39,36 +39,34 @@ function tabControl() {
       $(target).addClass('active');
     });
   }
+}
 
-  function mobileClickHandler() {
-    function makeItemActive() {
-      var container = $(this).parents('.tabbed-content'),
-          currId = $(this).attr('id'),
-          items = container.find('.item');
-      container.find('.tabs a').removeClass('active');
-      items.removeClass('active');
-      $(this).addClass('active');
-      container.find('.tabs a[href$="#' + currId + '"]').addClass('active');
-      $('html,body').animate({
-            scrollTop: $(this).offset().top - 80
-          },
-          'slow');
+function mobileClickHandler() {
+  $('.item').on('click', function () {
+    if ($(this).hasClass('active')) {
+      makeItemInactive.call(this);
+    } else {
+      makeItemActive.call(this);
     }
+  });
 
-    function makeItemInactive(){
-      var container = $(this).parents('.tabbed-content');
-      $(this).removeClass('active');
-      container.find('.tabs a.active').removeClass('active');
-    }
-
-    $('.item').on('click', function () {
-      if ($(this).hasClass('active')) {
-        makeItemInactive.call(this);
-      } else {
-        makeItemActive.call(this);
-      }
-    });
+  function makeItemActive() {
+    var container = $(this).parents('.tabbed-content'),
+        currId = $(this).attr('id'),
+        items = container.find('.item');
+    container.find('.tabs a').removeClass('active');
+    items.removeClass('active');
+    $(this).addClass('active');
+    container.find('.tabs a[href$="#' + currId + '"]').addClass('active');
+    $('html,body').animate({
+          scrollTop: $(this).offset().top - 80
+        },
+        'slow');
   }
 
-
+  function makeItemInactive() {
+    var container = $(this).parents('.tabbed-content');
+    $(this).removeClass('active');
+    container.find('.tabs a.active').removeClass('active');
+  }
 }
