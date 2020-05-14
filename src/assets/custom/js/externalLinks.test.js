3c4b77b58344d37c4e9b08eeb8ab1273693cfa71
Laurence Lord <laurence.lord@codurance.com>
2020-05-14 15:23:29 +0100

    Improve tests to check all links on the page

--- src/assets/custom/js/externalLinks.test.js
+++ src/assets/custom/js/externalLinks.test.js
@@ -7,17 +7,24 @@ const simulatePageLoad = () => {
 };
 
 describe("External Links", () => {
-  describe("When the page loads with external links", () => {
+  describe("When the page loads with two external links", () => {
     beforeEach(() => {
-      const externalLink = window.document.createElement("a");
-      externalLink.href = "https://test.com";
-      window.document.body.append(externalLink);
+      const externalLink1 = window.document.createElement("a");
+      externalLink1.id = "link1";
+      externalLink1.href = "https://test.com";
+      const externalLink2 = window.document.createElement("a");
+      externalLink2.id = "link2";
+      externalLink2.href = "https://another.com";
+      window.document.body.append(externalLink1);
+      window.document.body.append(externalLink2);
       simulatePageLoad();
     });
 
-    it(`should ensure the link opens in a new tab`, () => {
-      const link = window.document.querySelector("a");
-      expect(link.target).toBe("_blank");
+    it(`ensures both links open in new tabs`, () => {
+      const link1 = window.document.querySelector("#link1");
+      expect(link1.target).toBe("_blank");
+      const link2 = window.document.querySelector("#link2");
+      expect(link2.target).toBe("_blank");
     });
   });
 });
