981987c3b7d81d4bcd3225a7a626980a5959098c
Matt Gray <mattgray83@hotmail.co.uk>
2020-06-18 15:48:07 +0100

    Use data attributes instead of class names to switch between tabs

--- src/assets/custom/js/tabber.js
+++ src/assets/custom/js/tabber.js
@@ -9,7 +9,7 @@
     return;
   }
 
-  var TABS_WRAPPER = TABBER.querySelector(".tabs");
+  var TABS_WRAPPER = TABBER.querySelector("[data-tabs]");
   var TABS = nodeListToArray(TABS_WRAPPER.querySelectorAll("a"));
   var ITEMS = nodeListToArray(TABBER.querySelectorAll(".item"));
 
@@ -114,7 +114,7 @@
     }
     currentLayout = newLayout;
 
-    var noActiveTabs = document.querySelector(".tabs .active") === null;
+    var noActiveTabs = document.querySelector("[data-tabs] .active") === null;
 
     if (newLayout === "large" && noActiveTabs) {
       displaySustainableChangeTabContent();
