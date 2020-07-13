baf2848cd86ffcc5fe849a076ce1f0a496bb73e6
Cherry Tsui <cherry.tsui@codurance.com>
2020-07-13 10:24:48 +0100

    Add initial collapsed state of menu for small screens

--- src/assets/custom/js/websiteNavigation.js
+++ src/assets/custom/js/websiteNavigation.js
@@ -1,18 +1,37 @@
-var websiteNavigation = function(){
-    var NAVIGATION_MENU_SELECTOR = ".website-navigation__menu";
+var websiteNavigation = function () {
     var COLLAPSED_MENU_CLASS = "website-navigation__menu--collapsed";
+    var INITIAL_MENU_STATE_CLASS = "website-navigation__menu--initial-state";
 
-    var MENU_TOGGLE_SELECTOR = ".website-navigation__menu-toggle";
-    var menuToggleElement = window.document.querySelector(MENU_TOGGLE_SELECTOR);
-    menuToggleElement.addEventListener("click", toggleMenu);
+    var NAVIGATION_MENU_SELECTOR = ".website-navigation__menu";
+    var menuElement = window.document.querySelector(NAVIGATION_MENU_SELECTOR);
+
+    addMenuToggleEventListener();
+
+    function addMenuToggleEventListener() {
+        var MENU_TOGGLE_SELECTOR = ".website-navigation__menu-toggle";
+        var menuToggleElement = window.document.querySelector(MENU_TOGGLE_SELECTOR);
+        menuToggleElement.addEventListener("click", toggleMenu);
+    }
 
     function toggleMenu() {
-        var menuElement = window.document.querySelector(NAVIGATION_MENU_SELECTOR);
-        if(menuElement.classList.contains(COLLAPSED_MENU_CLASS)) {
-            menuElement.classList.remove(COLLAPSED_MENU_CLASS);
-        } else {
-            menuElement.classList.add(COLLAPSED_MENU_CLASS);
+        if (menuElement.classList.contains(INITIAL_MENU_STATE_CLASS)) {
+            menuElement.classList.remove(INITIAL_MENU_STATE_CLASS);
+            openMenu();
+            return;
         }
+        if (menuElement.classList.contains(COLLAPSED_MENU_CLASS)) {
+            openMenu();
+            return;
+        } 
+        collapseMenu();
+    }
+
+    function collapseMenu() {
+        menuElement.classList.add(COLLAPSED_MENU_CLASS);
+    }
+
+    function openMenu() {
+        menuElement.classList.remove(COLLAPSED_MENU_CLASS);
     }
 }
 
--- src/assets/custom/js/websiteNavigation.test.js
+++ src/assets/custom/js/websiteNavigation.test.js
@@ -2,54 +2,59 @@ const { simulatePageLoad } = require("./simulatePageLoad");
 require("./websiteNavigation");
 
 const COLLAPSED_MENU_CLASS = "website-navigation__menu--collapsed";
+const INITIAL_MENU_STATE_CLASS = "website-navigation__menu--initial-state";
 const MENU_CLASS = "website-navigation__menu";
-const MENU_TOGGLE_CLASS= "website-navigation__menu-toggle";
+const MENU_TOGGLE_CLASS = "website-navigation__menu-toggle";
 
-describe.only("Website Navigation Menu", () => {
-    let menuToggle;
-    let navMenu;
-    beforeAll(() => {
-        createMockMenuToggle();
-        createMockNav();
-        simulatePageLoad();
-        menuToggle = getMockMenuToggle();
-        navMenu = getMockNav();
-        navMenu.classList.forEach(e => console.log(e))
-    });
-    
-    it("stays collapsed when the menu toggle is not clicked", () => {
-        resetNav();
 
-        expect(navMenu.classList).toContain(COLLAPSED_MENU_CLASS);
-    });
+describe("Website Navigation Menu", () => {
 
-    it("is open when the menu toggle is clicked", () => {
-        resetNav();
+    describe("when the menu toggle is clicked", () => {
+        let menuToggle;
+        let navMenu;
+        beforeAll(() => {
+            setUpMocks();
+            simulatePageLoad();
+            menuToggle = getMockMenuToggle();
+            navMenu = getMockNav();
+        });
 
-        menuToggle.click();
+        it("is opened", () => {
+            closeNav();
 
-        expect(navMenu.classList).not.toContain(COLLAPSED_MENU_CLASS);
-    });
+            menuToggle.click();
 
-    it("is collapsed when the menu is initially open and the menu toggle is clicked", () => {
-        menuToggle.click();
-        
-        expect(navMenu.classList).toContain(COLLAPSED_MENU_CLASS);
+            expect(navMenu.classList).not.toContain(COLLAPSED_MENU_CLASS);
+            expect(navMenu.classList).not.toContain(INITIAL_MENU_STATE_CLASS);
+        });
+
+        it("is collapsed when the menu is initially open", () => {
+            openNav();
+
+            menuToggle.click();
+
+            expect(navMenu.classList).toContain(COLLAPSED_MENU_CLASS);
+        });
     });
 });
+
+function setUpMocks() {
+    createMockMenuToggle();
+    createMockNav();
+}
 
 function createMockMenuToggle() {
     const menuToggle = window.document.createElement("div");
     menuToggle.classList.add(MENU_TOGGLE_CLASS);
-    window.document.body.appendChild(menuToggle );
-  }
+    window.document.body.appendChild(menuToggle);
+}
 
 function createMockNav() {
     const navMenu = window.document.createElement("div");
     navMenu.classList.add(MENU_CLASS);
-    navMenu.classList.add(COLLAPSED_MENU_CLASS);
+    navMenu.classList.add(INITIAL_MENU_STATE_CLASS);
     window.document.body.appendChild(navMenu);
-  }
+}
 
 function getMockMenuToggle() {
     return window.document.querySelector(`.${MENU_TOGGLE_CLASS}`);
@@ -58,9 +63,10 @@ function getMockMenuToggle() {
 function getMockNav() {
     return window.document.querySelector(`.${MENU_CLASS}`);
 }
-function resetNav() {
-    let navMenu = getMockNav();
-    if(navMenu && !navMenu.classList.contains(COLLAPSED_MENU_CLASS)) {
-        navMenu.classList.add(COLLAPSED_MENU_CLASS);
-    }
+function closeNav() {
+    getMockNav().classList.add(COLLAPSED_MENU_CLASS);
+}
+
+function openNav() {
+    getMockNav().classList.remove(COLLAPSED_MENU_CLASS);
 }
