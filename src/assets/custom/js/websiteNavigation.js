c54811e38f1d69bd9b52c990bc8cded2fb507dd2
Laurence Lord <laurence.lord@codurance.com>
2020-07-13 14:26:02 +0100

    Inline click addEventListener

--- src/assets/custom/js/websiteNavigation.js
+++ src/assets/custom/js/websiteNavigation.js
@@ -1,32 +1,29 @@
 var websiteNavigation = function () {
-    var EXPANDED_MENU_CLASS = "website-navigation__menu--expanded";
+  var EXPANDED_MENU_CLASS = "website-navigation__menu--expanded";
 
-    var NAVIGATION_MENU_SELECTOR = ".website-navigation__menu";
-    var menuElement = window.document.querySelector(NAVIGATION_MENU_SELECTOR);
+  var NAVIGATION_MENU_SELECTOR = ".website-navigation__menu";
+  var menuElement = window.document.querySelector(NAVIGATION_MENU_SELECTOR);
 
-    addMenuToggleEventListener();
+  var MENU_TOGGLE_SELECTOR = ".website-navigation__menu-toggle";
+  var menuToggleElement = window.document.querySelector(MENU_TOGGLE_SELECTOR);
 
-    function addMenuToggleEventListener() {
-        var MENU_TOGGLE_SELECTOR = ".website-navigation__menu-toggle";
-        var menuToggleElement = window.document.querySelector(MENU_TOGGLE_SELECTOR);
-        menuToggleElement.addEventListener("click", toggleMenu);
+  menuToggleElement.addEventListener("click", toggleMenu);
+
+  function toggleMenu() {
+    if (menuElement.classList.contains(EXPANDED_MENU_CLASS)) {
+      collapseMenu();
+      return;
     }
+    expandMenu();
+  }
 
-    function toggleMenu() {
-        if (menuElement.classList.contains(EXPANDED_MENU_CLASS)) {
-            collapseMenu();
-            return;
-        } 
-        expandMenu();
-    }
+  function collapseMenu() {
+    menuElement.classList.remove(EXPANDED_MENU_CLASS);
+  }
 
-    function collapseMenu() {
-        menuElement.classList.remove(EXPANDED_MENU_CLASS);
-    }
-
-    function expandMenu() {
-        menuElement.classList.add(EXPANDED_MENU_CLASS);
-    }
-}
+  function expandMenu() {
+    menuElement.classList.add(EXPANDED_MENU_CLASS);
+  }
+};
 
 websiteNavigation();
