d6faab74b0d97183fa70a3c3cdb449f1c1d118e4
Laurence Lord <laurence.lord@codurance.com>
2020-07-14 18:02:07 +0100

    Transition to slate background

--- src/assets/custom/js/websiteNavigation.js
+++ src/assets/custom/js/websiteNavigation.js
@@ -1,7 +1,11 @@
 var websiteNavigation = function () {
-  var OPEN_MENU_CLASS = "website-navigation__menu--open";
+  var HEADER_SELECTOR = ".website-header";
   var MENU_TOGGLE_SELECTOR = ".website-navigation-menu-toggle";
 
+  var OPEN_HEADER_CLASS = "website-header--open";
+  var OPEN_MENU_CLASS = "website-navigation__menu--open";
+
+  var header = window.document.querySelector(HEADER_SELECTOR);
   var menuToggle = window.document.querySelector(MENU_TOGGLE_SELECTOR);
   var menu = window.document.querySelector(
     "#" + menuToggle.getAttribute("aria-controls")
@@ -14,11 +18,13 @@ var websiteNavigation = function () {
   }
 
   function closeMenu() {
+    header.classList.remove(OPEN_HEADER_CLASS);
     menuToggle.setAttribute("aria-expanded", "false");
     menu.classList.remove(OPEN_MENU_CLASS);
   }
 
   function openMenu() {
+    header.classList.add(OPEN_HEADER_CLASS);
     menuToggle.setAttribute("aria-expanded", "true");
     menu.classList.add(OPEN_MENU_CLASS);
   }
