d9f598922a89afeaec3f42ed0d3df0ccd69ec2ab
Daniel <info@daniel-bird.co.uk>
2020-07-16 22:32:14 +0100

    Setup initial event handlers

--- src/assets/custom/js/training-card-group.js
+++ src/assets/custom/js/training-card-group.js
@@ -1,3 +1,18 @@
 (function() {
+  var TRACK_SELECTOR = "[data-nav-track]";
+  var NAV_ITEM_SELECTOR = "[data-nav-item]";
+
+  var track = document.querySelectorAll(TRACK_SELECTOR);
+  var navItems = Array.prototype.slice.call(document.querySelectorAll(NAV_ITEM_SELECTOR));
+
+  navItems.forEach(function(item) {
+    item.addEventListener('click', handleNavItemClick);
+  });
+
+  function handleNavItemClick(e) {
+    console.log('clicked');
+
+  }
+
 
 })();
