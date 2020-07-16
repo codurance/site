a4f9687c20da8c80f7e97ffdb21ab7923262bb0a
Daniel <info@daniel-bird.co.uk>
2020-07-16 23:05:14 +0100

    Highlight correct NavItem on click

--- src/assets/custom/js/training-card-group.js
+++ src/assets/custom/js/training-card-group.js
@@ -1,6 +1,7 @@
 (function() {
   var TRACK_SELECTOR = "[data-nav-track]";
   var NAV_ITEM_SELECTOR = "[data-nav-item]";
+  var ACTIVE_CLASS = 'active'
 
   var track = document.querySelectorAll(TRACK_SELECTOR);
   var navItems = Array.prototype.slice.call(document.querySelectorAll(NAV_ITEM_SELECTOR));
@@ -11,9 +12,25 @@
 
   function handleNavItemClick(e) {
     var position = e.target.dataset.position;
-    console.log(position);
+    goToPosition(position);
+  }
+
+  function goToPosition(position) {
+    positionCard(position);
+    highlightNavItem(position);
+  }
+
+  function positionCard(position) {
+    console.log(`positionCard: `, position);
 
   }
 
+  function highlightNavItem(position) {
+    var currentlyActive = document.querySelector(NAV_ITEM_SELECTOR + '.' + ACTIVE_CLASS);
+    var newlyActive = navItems[position - 1];
+
+    currentlyActive.classList.remove(ACTIVE_CLASS);
+    newlyActive.classList.add(ACTIVE_CLASS);
+  }
 
 })();
