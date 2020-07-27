01d86622ed47d5633583c525fdafc1c78a061fc7
Daniel <info@daniel-bird.co.uk>
2020-07-27 11:32:14 +0100

    Move further code to shared file

--- src/assets/custom/js/websiteHeaderShared.js
+++ src/assets/custom/js/websiteHeaderShared.js
@@ -1,9 +1,29 @@
 var HEADER_SELECTOR = ".website-header";
 var HEADER_HAS_OPEN_SUBMENU_CLASS = "website-header--has-open-submenu";
+var MENU_TOGGLE_SELECTOR = ".website-navigation-menu-toggle";
+var MENU_SHOWING_SUB_MENU_CLASS =
+"website-navigation__menu--showing-sub-menu";
+var OPEN_SUB_MENU_CLASS = "website-navigation-sub-menu--open";
 
 var header = window.document.querySelector(HEADER_SELECTOR);
+var menuToggle = window.document.querySelector(MENU_TOGGLE_SELECTOR);
+var menu = window.document.querySelector(
+  "#" + menuToggle.getAttribute("aria-controls")
+);
 
+var currentOpenSubMenu = null;
 
 var getScrollPosition = function () {
   return window.pageYOffset || window.scrollY;
 };
+
+function closeSubMenu(subMenu, subMenuToggle) {
+  currentOpenSubMenu = null;
+
+  header.classList.remove(HEADER_HAS_OPEN_SUBMENU_CLASS);
+  subMenuToggle.setAttribute("aria-expanded", "false");
+  subMenu.classList.remove(OPEN_SUB_MENU_CLASS);
+  menu.classList.remove(MENU_SHOWING_SUB_MENU_CLASS);
+
+  subMenuToggle.style.removeProperty('margin-bottom');
+}
--- src/assets/custom/js/websiteNavigation.js
+++ src/assets/custom/js/websiteNavigation.js
@@ -1,27 +1,14 @@
 var websiteNavigation = function () {
-
-  var MENU_TOGGLE_SELECTOR = ".website-navigation-menu-toggle";
   var SUB_MENU_TOGGLE_SELECTOR = ".website-navigation-menu__sub-menu-toggle";
   var SUB_MENU_TOGGLE_PROXY_SELECTOR =
     ".website-navigation-sub-menu__toggle-proxy";
-
-  var MENU_SHOWING_SUB_MENU_CLASS =
-    "website-navigation__menu--showing-sub-menu";
-
   var OPEN_HEADER_CLASS = "website-header--open";
   var OPEN_MENU_CLASS = "website-navigation__menu--open";
-  var OPEN_SUB_MENU_CLASS = "website-navigation-sub-menu--open";
 
-  var menuToggle = window.document.querySelector(MENU_TOGGLE_SELECTOR);
-  var menu = window.document.querySelector(
-    "#" + menuToggle.getAttribute("aria-controls")
-  );
   var subMenuToggles = menu.querySelectorAll(SUB_MENU_TOGGLE_SELECTOR);
   var subMenuToggleProxies = window.document.querySelectorAll(
     SUB_MENU_TOGGLE_PROXY_SELECTOR
   );
-
-  var currentOpenSubMenu = null;
 
   function setupEventListeners() {
     window.addEventListener("click", handleWindowClick);
@@ -106,17 +93,6 @@ var websiteNavigation = function () {
     menu.classList.add(MENU_SHOWING_SUB_MENU_CLASS);
 
     subMenuToggle.style.marginBottom = subMenu.scrollHeight + "px";
-  }
-
-  function closeSubMenu(subMenu, subMenuToggle) {
-    currentOpenSubMenu = null;
-
-    header.classList.remove(HEADER_HAS_OPEN_SUBMENU_CLASS);
-    subMenuToggle.setAttribute("aria-expanded", "false");
-    subMenu.classList.remove(OPEN_SUB_MENU_CLASS);
-    menu.classList.remove(MENU_SHOWING_SUB_MENU_CLASS);
-
-    subMenuToggle.style.removeProperty('margin-bottom');
   }
 
   function getSubMenu(subMenuToggle) {
