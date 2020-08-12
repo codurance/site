1f72d7a6e6da74cf4c647cfd2ba3700076e22d26
Matt Gray <mattgray83@hotmail.co.uk>
2020-08-12 12:07:05 +0100

    Clean up JS and make it toggle read more/read less text

--- src/assets/custom/js/training-instructor-bio.js
+++ src/assets/custom/js/training-instructor-bio.js
@@ -1,10 +1,25 @@
 (function () {
+    var CLOSED_TEXT = "read more";
+    var OPEN_TEXT = "read less";
     var button = document.querySelector(".training-bio__read-more-button");
-    button.addEventListener("click", readMore);
+    var training_bio = document.querySelector(".training-bio");
 
-    var content = document.querySelector(".training-bio");
+    button.addEventListener("click", handleClick);
 
-    function readMore() {
-        content.classList.toggle("training-bio--open");
+    function handleClick() {
+        updateClass();
+        updateButtonText();
+    }
+
+    function updateClass() {
+        if (training_bio) {
+            training_bio.classList.toggle("training-bio--open");
+        }
+    }
+
+    function updateButtonText() {
+        if (button) {
+            button.textContent = training_bio.classList.contains("training-bio--open") ? OPEN_TEXT : CLOSED_TEXT;
+        }
     }
 })();
