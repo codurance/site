8873322ae4a05f9db786ae330495238e6f9d33e8
Matt Gray <mattgray83@hotmail.co.uk>
2020-09-22 17:51:53 +0100

    Try changing order of exclude commands

--- build/_utility/deploy_es_site.sh
+++ build/_utility/deploy_es_site.sh
@@ -3,5 +3,5 @@
 set -e
 
 cd output/_site_es/
-aws s3 sync . s3://codurance-website-es --exclude "assets/*" --acl public-read --delete --exclude "services/*"
+aws s3 sync . s3://codurance-website-es --exclude "assets/*" --exclude "services/*" --acl public-read --delete
 aws cloudfront create-invalidation --distribution-id E12HY50RS84ICW --paths "/*"
