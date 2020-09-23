78bc3d5d8458d7dd630f3fff2c3f73aca490ecf0
Matt Gray <mattgray83@hotmail.co.uk>
2020-09-23 15:33:59 +0100

    Wrong directory

--- build/_utility/deploy_prb.sh
+++ build/_utility/deploy_prb.sh
@@ -15,7 +15,7 @@ cd output/_site/
 echo "Synchronizing to folder: $FOLDER_NAME"
 aws s3 sync . s3://codurance-site-pr/$FOLDER_NAME --exclude "*/assets/*" --acl public-read --delete
 
-cd ../../../redirects-for-spanish-services-pages/
+cd ../../redirects-for-spanish-services-pages/
 aws s3 sync . s3://codurance-site-pr/$FOLDER_NAME/es/services --acl public-read --delete
 
 aws cloudfront create-invalidation --distribution-id EML2BUMD54HSC --paths "/$FOLDER_NAME/*"
