09a2b4aab1b1bcb91f4e83fed0a91ae213746809
Matt Gray <mattgray83@hotmail.co.uk>
2020-09-23 14:48:20 +0100

    Test - deploy straight into PR bucket so that we can test it on the preview branch

--- build/_utility/deploy_es_site.sh
+++ build/_utility/deploy_es_site.sh
@@ -6,6 +6,6 @@ cd output/_site_es/
 aws s3 sync . s3://codurance-website-es --exclude "assets/*" --exclude "services/*" --acl public-read --delete
 
 cd redirects-for-spanish-services-pages/
-aws s3 sync . s3://codurance-website-es/services --acl public-read --delete
+aws s3 sync . s3://codurance-site-pr/site-change-urls-based-on-site-language/es/services --acl public-read
 
 aws cloudfront create-invalidation --distribution-id E12HY50RS84ICW --paths "/*"
