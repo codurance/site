fa28826ab2d3f15833f05dbda401a765248dc6b2
Matt Gray <mattgray83@hotmail.co.uk>
2020-09-24 15:23:37 +0100

    Correct directories

--- build/_utility/deploy_es_site.sh
+++ build/_utility/deploy_es_site.sh
@@ -5,7 +5,7 @@ set -e
 cd output/_site_es/
 aws s3 sync . s3://codurance-website-es --exclude "assets/*" --exclude "services/*" --acl public-read --delete
 
-cd ../../../redirects-for-spanish-services-pages/
+cd ../../redirects-for-spanish-services-pages/
 aws s3 sync . s3://codurance-website-es/services --acl public-read --delete
 
 aws cloudfront create-invalidation --distribution-id E12HY50RS84ICW --paths "/*"
